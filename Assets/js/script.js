609524071cc3abad72263378910981014a96bc65
EduardB <eduard.bahrin@yahoo.com>
2022-10-31 20:14:50 +0000

    added all timeblocks for the  local storage function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -25,10 +25,16 @@ function setColor(hour) {
 var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 for (var i = 0; i < hours.length; i++) {
   setColor(hours[i]);
+  loadBlock(hours[i]);
 }
 // when the save button is pressed data is saved in local storage
 function saveBlock(id) {
   var blockContent = $("#" + id).val();
 
   localStorage.setItem("blockContent" + id, blockContent);
+}
+
+function loadBlock(id) {
+  var blockContent = localStorage.getItem("blockContent" + id);
+  $("#" + id).val(blockContent);
 }
